63bda85e3d59d1a85eb43fad5b8ca01ac2c1b0dc
Zhaoyu Wang <wzy0794@gmail.com>
2022-09-24 21:21:39 -0400

    Optimize server

--- communication/server.py
+++ communication/server.py
@@ -19,10 +19,10 @@ class GossipServer(multiprocessing.Process):
         self.connection_pool = connection_pool
         self.adversarial_controller = adversarial_controller
         self.state = initial_state
+        self._server_lock = multiprocessing.Lock()
 
     def run(self):
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             server_socket.bind((self.ip_address, self.tcp_port))
             server_socket.listen(5)
             self.connection_pool.update_connection(
@@ -31,10 +31,12 @@ class GossipServer(multiprocessing.Process):
 
             while True:
                 try:
+                    self._server_lock.acquire()
                     client_socket, client_address = server_socket.accept()
                     client_ip, client_port = client_address
                     client_identfier = "{}:{}".format(client_ip, client_port)
                     self.adversarial_controller.response(client_socket, client_identfier)
+                    self._server_lock.release()
                 except Exception as e:
                     print(
                         "[SERVER]: Failed to response to (%s) server due to %s..." % (client_socket, e))
